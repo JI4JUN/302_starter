5128b7e6e5da43b38cc9793409ded80bd0a17034
mthezi <mthezi@163.com>
2025-01-08 17:57:14 +0800

    fix: 1px error

--- src/components/global/app-header/index.tsx
+++ src/components/global/app-header/index.tsx
@@ -18,7 +18,7 @@ const Header = forwardRef<HTMLDivElement, HeaderProps>(({ className }, ref) => {
   return (
     <header
       className={cn(
-        "border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60",
+        "bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60",
         className
       )}
     >
